1ac86ccb63eea57cb0d9e1e76a09d572c1a98de0
Zulkarnaen <Zulkarnaim70@gmail.com>
2022-07-04 09:30:35 +0700

    change base url website

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://magerngulik.my.id/';
+$config['base_url'] = 'https://magerngulik.my.id/pustaka';
 
 /*
 |--------------------------------------------------------------------------
